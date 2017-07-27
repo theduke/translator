067312743b92640c1983add37cb00541de6ee8f0
Christoph Herzog <christoph.herzog@ready2order.com>
2017-07-27 17:06:33 +0200

    Added Dockerfile and docker build/publish script.

---
+++ bin/publish-docker-image.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+
+$(cd backend && cargo clean)
+
+# Go to project dir.
+echo `pwd`
+docker build -f docker/Dockerfile -t theduke/translator:latest .
+docker publish theduke/translator:latest
